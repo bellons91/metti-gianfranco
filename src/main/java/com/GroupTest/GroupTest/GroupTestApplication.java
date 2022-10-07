1e54cf4ae4b475f7313ca8eeee857d702fd59f7a
Andrea Demontis <andreademontis.jobs@gmail.com>
2022-10-07 10:43:34 +0200

    FirstCommit

---
+++ src/main/java/com/GroupTest/GroupTest/GroupTestApplication.java
@@ -0,0 +1,13 @@
+package com.GroupTest.GroupTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTestApplication.class, args);
+	}
+
+}
