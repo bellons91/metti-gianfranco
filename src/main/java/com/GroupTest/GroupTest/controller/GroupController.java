3edf8179befea09afc0dd11062eb5ae5b9bc8966
emili4ciccone <emiliaciccone@gmail.com>
2022-10-07 11:55:02 +0200

    Controller

--- src/main/java/com/GroupTest/GroupTest/controller/GroupController.java
+++ src/main/java/com/GroupTest/GroupTest/controller/GroupController.java
@@ -1,7 +1,18 @@
 package com.GroupTest.GroupTest.controller;
 
+import com.GroupTest.GroupTest.book.IBookService;
+import com.GroupTest.GroupTest.weather.IWeatherService;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class GroupController {
+
+    private IWeatherService weatherService;
+    private IBookService bookService;
+    public GroupController(IWeatherService weatherService, IBookService bookService) {
+        this.weatherService = weatherService;
+        this.bookService = bookService;
+    }
+
 }
