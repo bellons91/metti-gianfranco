4f8c49397a7ae032d980004d2ce36ae659cc3cbf
Stefano Baviello <stefano.baviello@teoresigroup.com>
2022-10-08 21:29:37 +0200

    tests & implementation BookService

--- src/main/java/com/GroupTest/GroupTest/book/BookService.java
+++ src/main/java/com/GroupTest/GroupTest/book/BookService.java
@@ -1,12 +1,62 @@
 package com.GroupTest.GroupTest.book;
 
+import com.google.gson.*;
+import java.util.*;
+
 public class BookService implements IBookService{
+    private final HttpBook httpBook;
+
+    public BookService(HttpBook httpBook) {
+        this.httpBook = httpBook;
+    }
+
     @Override
-    public Book getBooks(String cityName) {
-        //chiama mnetodo
+    public ArrayList<Book> getBooks(String cityName) {
 
-        //parsifica jsonb in book
+        if (cityName == null || cityName.isEmpty() || cityName.isBlank()) throw new IllegalArgumentException("Parametro invalido");
 
-        return null;
+        String json = (String) httpBook.getResult(cityName).body();
+
+        ArrayList<Book> res = parseResult(json);
+
+        if (res.isEmpty()){
+            throw new IllegalArgumentException("Nessun libro con la città specificata: " + cityName);
+        }
+
+        return res;
+    }
+
+    private ArrayList<Book> parseResult(String json) {
+        ArrayList<Book> result = new ArrayList<>();
+
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+
+        jsonObject.remove("count");
+        jsonObject.remove("next");
+        jsonObject.remove("previous");
+
+        JsonArray allBooks = jsonObject.getAsJsonArray("results");
+        for (JsonElement elem : allBooks){
+            elem.getAsJsonObject().remove("translators");
+            elem.getAsJsonObject().remove("subjects");
+            elem.getAsJsonObject().remove("bookshelves");
+            elem.getAsJsonObject().remove("languages");
+            elem.getAsJsonObject().remove("copyright");
+            elem.getAsJsonObject().remove("media_type");
+            elem.getAsJsonObject().remove("formats");
+            elem.getAsJsonObject().remove("download_count");
+
+            JsonArray authors = elem.getAsJsonObject().getAsJsonArray("authors");
+            for (JsonElement auth : authors){
+                auth.getAsJsonObject().remove("birth_year");
+                auth.getAsJsonObject().remove("death_year");
+            }
+        }
+
+
+        for (int i=0;i<allBooks.size();i++)
+            result.add(new Gson().fromJson(allBooks.get(i), Book.class));
+
+        return result;
     }
 }
